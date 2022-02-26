6d3f7fbb40b2a79b9453b2f75b2441b516be6aa6
Sergio Núñez <sergio-n-g@hotmail.com>
2022-02-25 19:08:56 -0600

    Move the proto file to a version folder
    Try v2 folder for proto files (version proto)

--- script/protoc.sh
+++ script/protoc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PROTO_DIR=goreuse/test
+PROTO_DIR=goreuse/test/v2
 
 protoc --proto_path=${PROTO_DIR} --go_out=. --go-grpc_out=. \
     --go_opt=module=github.com/sergionunezgo/go-reuse/v2 \
